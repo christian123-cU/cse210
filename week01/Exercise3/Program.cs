8c7d32c46c85fcdae5de5bba2308adc32bd04d3d
christian123-cU <ochandadavid2000@gmail.com>
2026-07-04 20:03:52 +0300

    User magic number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Ask the user for the magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Ask the user for their guess
+        Console.Write("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+
+        // Determine if the guess is correct, too high, or too low
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
