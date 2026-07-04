e0ea25fe820015f3b08d6a9975ac0a65e5844aba
christian123-cU <ochandadavid2000@gmail.com>
2026-07-04 20:12:00 +0300

    final editing of exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,32 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask the user for the magic number
-        Console.Write("What is the magic number? ");
-        int magicNumber = Convert.ToInt32(Console.ReadLine());
+        Random random = new Random();
+        string playAgain = "yes";
 
-        // Ask the user for their first guess
-        Console.Write("What is your guess? ");
-        int guess = Convert.ToInt32(Console.ReadLine());
-
-        // Keep looping as long as the guess does not match the magic number
-        while (guess != magicNumber)
+        // Keep playing the whole game as long as the user says "yes"
+        while (playAgain.ToLower() == "yes")
         {
-            if (guess < magicNumber)
+            // Generate a random magic number from 1 to 100
+            int magicNumber = random.Next(1, 101);
+            int guessCount = 0;
+
+            // Ask the user for their first guess
+            Console.Write("What is your guess? ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            guessCount++;
+
+            // Keep looping as long as the guess does not match the magic number
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("Lower");
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                // Ask for another guess
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
             }
 
-            // Ask for another guess
-            Console.Write("What is your guess? ");
-            guess = Convert.ToInt32(Console.ReadLine());
+            // Once the loop ends, the guess must match the magic number
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine("It took you " + guessCount + " guesses.");
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
         }
 
-        // Once the loop ends, the guess must match the magic number
-        Console.WriteLine("You guessed it!");
+        Console.WriteLine("Thanks for playing!");
     }
 }
