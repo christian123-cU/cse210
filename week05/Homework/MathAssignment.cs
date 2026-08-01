dc5c3ce48f805ae6ddc3a4e2cadbfd1a7f6d779f
christian123-cU <ochandadavid2000@gmail.com>
2026-08-01 18:58:03 +0300

    The coded assignment

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _studentName;
+    private string _topic;
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return "Section " + _textbookSection + " Problems " + _problems;
+    }
+}
